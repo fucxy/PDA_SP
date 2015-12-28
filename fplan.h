5913498c368f852e7f2e65e7894badda19b88e71
fucxy <innocencei.m@gmail.com>
2015-12-28 10:29:13 +0000

    modify fplan.h

--- fplan.h
+++ fplan.h
@@ -1,1 +1,18 @@
+#ifndef fplanH
+#define fplanH
+//--------------------------------------------------------------------------
+#include <vector>
+#include <string>
+#include <fstream>
+#include <map>
+#include <cstdio>
+//--------------------------------------------------------------------------
+using namespace std;
 
+struct Pin{
+  int mod;
+  int net;
+  int x,y;
+  int ax,ay;
+  Pin(int x_=-1,int y_=-1){ x=x_,y=y_; }
+};
