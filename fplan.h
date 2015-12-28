ede0bb8fc27ac010665d4fb4c7513b416e12aec7
fucxy <innocencei.m@gmail.com>
2015-12-28 10:53:37 +0000

    modify fplan.h

--- fplan.h
+++ fplan.h
@@ -16,3 +16,26 @@ struct Pin{
   int ax,ay;
   Pin(int x_=-1,int y_=-1){ x=x_,y=y_; }
 };
+typedef vector<Pin> Pins;
+typedef Pin* Pin_p;
+typedef vector<Pin_p> Net;
+typedef vector <Net> Nets;
+
+struct Module{
+  int id;
+  char name[20];
+  int width,height;
+  int x=0,y=0;
+  int area;
+  Pins pins;
+};
+typedef vector<Module> Modules;
+
+struct Module_Info{
+  bool rotate;
+  int x,y;
+  int rx,ry;
+};
+
+typedef vector<Module_Info> Modules_Info;
+
