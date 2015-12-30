f24e0c1392054bb235fae0edd867b3af5d96fd5e
fucxy <innocencei.m@gmail.com>
2015-12-30 08:05:48 +0000

    fplan.cc read function (unfinished)

--- fplan.cc
+++ fplan.cc
@@ -58,7 +58,57 @@ void FPlan::read(const char *block_file,const char *net_file,const char* rf_file
   cout<<"Read file"<<endl;
   int ta,tb,th,tw;
   filename = rt_file;
-  fs.open(block_file);
+  fs.open(block_file,ios::in);
+  if(fs==NULL)
+    error("unable to open block file","");
+  fs >> t1 >> max_Width >> max_Height;
+  fs >> t1 >>ta;
+  fs >> t1 >>tb;
+  bool final = false;
+  Module dummy_mod;
+  for(int i=0;i<ta;i++){
+    modules.push_back(dummy_mod);
+    Module &mod = modules.back();
+    mod.id = i;
+    mod.pins.clear();
+    fs >> t1 >> mod.width >> mod.height;
+    strcpy(mod.name,t1);
+    mod.area = mod.width * mod.height;
+    Pin p;
+    p.mod = i;
+    p.x = mod.width/2;
+    p.y = mod.height/2;
+    p.net = INT_MAX;
+    mod.pins.push_back(p);
+  }
+  //root terminal module
+  root_module.id = ta;
+  root_module.pins.clear();
+  strcpy(root_module.name,"terminal");
+  root_module.width = max_Width;
+  root_module.heigth = max_Height;
+  root_module.area = max_Width * max_Height;
+  if(tb > 0)
+    for(int i=0;i<tb;i++){
+      fs >> t1 >> t2 >> tw >>th;
+      Pin p;
+      p.mod = to;
+      p.x = tw;
+      p.y = th;
+      p.net = INT_MAX;
+      root_module.pins.push_back(p);
+      net_table.insert(make_pair(string(t1),net_table.size()));
+    }
+  modules.push_back(root_module);
+  fs.close();
+  //finish block file
+
+  //read .net
+  fs.pen(net_file,ios::in);
+  fs >> t1 >> ta;
+  for(int i=0;i<ta;i++){
+    
+  }
 }
 
 //-----------------
